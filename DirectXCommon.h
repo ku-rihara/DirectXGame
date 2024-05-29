c1d0ec713b5c103013c2b77041eb16a04970cc5f
ku-rihara <k023g0027@g.neec.ac.jp>
2024-05-30 01:44:31 +0900

    ヘッダー整理

--- DirectXCommon.h
+++ DirectXCommon.h
@@ -66,34 +66,36 @@ private://メンバ変数
 	IDxcBlob* vertexShaderBlob_;
 	IDxcBlob* pixelShaderBlob_;
 	ID3D12PipelineState* graphicsPipelineState_;
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_;
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite_;
-
+	
 	//球
 	const uint32_t kSubdivision_ = 16;//分割数
 	const uint32_t  shpereVertexNum_ = 1536;
 
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_;
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite_;
+	D3D12_INDEX_BUFFER_VIEW indexBufferView_{};
+	D3D12_INDEX_BUFFER_VIEW indexBufferViewSprite_{};
 	//リソース******************************************************************
 	//Material
 	ID3D12Resource* materialResource_;
 	ID3D12Resource* materialResourceSprite_;
 	//平行光源
 	ID3D12Resource* directionalLightResource_;
-	ID3D12Resource* directionalLightResourceSprite_;
-	DirectionalLight* directionalLightData_;
+	ID3D12Resource* directionalLightResourceSprite_;	
 	//頂点リソース
 	ID3D12Resource* vertexResource_;
 	ID3D12Resource* vertexResourceSprite_;
 	//wvpリソース
 	ID3D12Resource* wvpResouce_;
 	ID3D12Resource* wvpResourceSprite_;
-	TransformationMatrix* wvpDate_;
-	TransformationMatrix* wvpDataSprite_;
 	//indexリソース
 	ID3D12Resource* indexResource_;
-	D3D12_INDEX_BUFFER_VIEW indexBufferView_{};
 	ID3D12Resource* indexResourceSprite_;
-	D3D12_INDEX_BUFFER_VIEW indexBufferViewSprite_{};
+	
+	//データ****************************************************************************
+	TransformationMatrix* wvpDate_;
+	TransformationMatrix* wvpDataSprite_;
+	DirectionalLight* directionalLightData_;
 
 	//DescriptorSize
 	uint32_t descriptorSizeSRV_;
