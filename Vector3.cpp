1865bafb3b03b735a7e36805dac2c5b1a9f57429
ku-rihara <k023g0027@g.neec.ac.jp>
2024-08-02 10:10:51 +0900

    Vector3変更

--- Vector3.cpp
+++ Vector3.cpp
@@ -1,6 +1,106 @@
-#include"Vector3.h"
+
+#include "Vector3.h"
 #include<math.h>
 
+
+const float kColumnWidth = 60;
+
+
+
+
+Vector3 Vector3:: operator+(const Vector3& obj)  const {
+
+	Vector3 result;
+
+	result.x = x + obj.x;
+	result.y = y + obj.y;
+	result.z = z + obj.z;
+
+	return result;
+
+}
+
+Vector3 Vector3:: operator-(const Vector3& obj) const{
+
+	Vector3 result;
+
+	result.x = x - obj.x;
+	result.y = y - obj.y;
+	result.z = z - obj.z;
+
+	return result;
+
+}
+
+Vector3 Vector3:: operator*(const Vector3& obj) const {
+
+	Vector3 result;
+
+	result.x = x * obj.x;
+	result.y = y * obj.y;
+	result.z = z * obj.z;
+
+	return result;
+
+}
+
+Vector3 Vector3:: operator*(const float& obj)  const {
+	Vector3 result;
+
+	result.x = x * obj;
+	result.y = y * obj;
+	result.z = z * obj;
+	
+	return result;
+}
+
+Vector3 Vector3:: operator/(const Vector3& obj)  const {
+
+	Vector3 result;
+
+	if (obj.x != 0) {
+		result.x = x / obj.x;
+
+	}
+	else {
+		result.x = 0;
+
+	}
+
+	if (obj.y != 0) {
+		result.y = y / obj.y;
+
+	}
+	else {
+		result.y = 0;
+
+	}
+
+	if (obj.z != 0) {
+		result.z = z / obj.z;
+
+	}
+	else {
+		result.z = 0;
+
+	}
+
+	return result;
+
+}
+
+
+//内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+//長さ（ノルム）
+float Length(const Vector3& v) {
+	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+//正規化
 Vector3 Normnalize(const Vector3& v) {
 	Vector3 result;
 
@@ -15,5 +115,22 @@ Vector3 Normnalize(const Vector3& v) {
 		result.y = 0;
 		result.z = 0;
 	}
+	return result;
+}
+
+Vector3 Cross(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+
+	result.x = (v1.y * v2.z) - (v1.z * v2.y);
+	result.y = (v1.z * v2.x) - (v1.x * v2.z);
+	result.z = (v1.x * v2.y) - (v1.y * v2.x);
+	return result;
+}
+
+Vector3 Multiply(const Vector3& v1, const float& v2) {
+	Vector3 result;
+	result.x = v1.x * v2;
+	result.y = v1.y * v2;
+	result.z = v1.z * v2;
 	return result;
 }
--- Vector3.h
+++ Vector3.h
@@ -1,11 +1,30 @@
-#pragma once
-/// <summary>
-/// 3次元ベクトル
-/// </summary>
+﻿#pragma once
 struct Vector3 {
-	float x;
-	float y;
-	float z;
+    float x;
+    float y;
+    float z;
+
+    Vector3() : x(0), y(0), z(0) {}
+    Vector3(float numX, float numY, float numZ) : x(numX), y(numY), z(numZ) {}
+
+    Vector3 operator+(const Vector3& obj)const;
+
+    Vector3 operator-(const Vector3& obj)const;
+
+    Vector3 operator*(const Vector3& obj)const;
+    Vector3 operator*(const float& scalar)const;
+
+    Vector3 operator/(const Vector3& obj)const;
 };
 
-Vector3 Normnalize(const Vector3& v);
+Vector3 Normnalize(const Vector3& v);  //正規化
+
+Vector3 Multiply(const Vector3&v1, const float& v2);
+
+Vector3 Cross(const Vector3& v1, const Vector3& v2);  //クロス積
+
+float Dot(const Vector3& v1, const Vector3& v2);  //内積
+
+float Length(const Vector3& v);   //長さ（ノルム）
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
