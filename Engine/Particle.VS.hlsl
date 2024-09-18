4309099dfcda9bb10b4939abf481e133335a038f
ku-rihara <k023g0027@g.neec.ac.jp>
2024-09-19 00:18:03 +0900

    PS書き換え

--- Engine/Particle.VS.hlsl
+++ Engine/Particle.VS.hlsl
@@ -15,7 +15,7 @@ StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 VertexShaderOutput main(VertexShaderInput input,uint instanceID:SV_InstanceID)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, gTransformationMatrices[instanceID].World);
+    output.position = mul(input.position, gTransformationMatrices[instanceID].WVP);
     output.texcoord = input.texcoord;
     output.normal = normalize(mul(input.normal, (float3x3) gTransformationMatrices[instanceID].World));
     
