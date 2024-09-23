bf36975a21b03706c5b204a72ca128ffa8d02ef1
ku-rihara <k023g0027@g.neec.ac.jp>
2024-09-23 22:19:16 +0900

    非均一スケール

--- Engine/Object3d.VS.hlsl
+++ Engine/Object3d.VS.hlsl
@@ -9,6 +9,7 @@ struct TransformationMatrix
 {
     float4x4 WVP;
     float4x4 World;
+    float4x4 WorldInverseTranspose;
 };
 
 ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
@@ -17,7 +18,7 @@ ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 VertexShaderOutput main(VertexShaderInput input){
 VertexShaderOutput output;
-    output.normal = normalize(mul(input.normal, (float3x3)gTransformationMatrix.World));
+    output.normal = normalize(mul(input.normal, (float3x3)gTransformationMatrix.WorldInverseTranspose));
     output.position = mul(input.position,gTransformationMatrix.WVP);
     output.worldPosition = mul(input.position, gTransformationMatrix.World).xyz;
     output.texcoord = input.texcoord;
--- math/Matrix4x4.cpp
+++ math/Matrix4x4.cpp
@@ -73,30 +73,6 @@ void Matrix4x4::operator*=(const Matrix4x4& obj) {
 
 
 
-//Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
-//	Matrix4x4 result;
-//
-//	for (int row = 0; row < 4; ++row) {
-//
-//		for (int column = 0; column < 4; ++column) {
-//
-//			result.m[row][column] = m1.m[row][0] * m2.m[0][column] + m1.m[row][1] * m2.m[1][column] + m1.m[row][2] * m2.m[2][column] + m1.m[row][3] * m2.m[3][column];
-//		}
-//	}
-//
-//	return result;
-//}
-//
-//Vector3 Multiply(const Vector3& vector, const Matrix4x4& matrix) {
-//	Vector3 result;
-//
-//	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + matrix.m[3][0];
-//	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + matrix.m[3][1];
-//	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + matrix.m[3][2];
-//
-//	return result;
-//}
-
 Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
 	Matrix4x4 result;
 
@@ -300,6 +276,18 @@ Matrix4x4 Inverse(const Matrix4x4& m) {
 		(m.m[0][1] * m.m[1][0] * m.m[3][2]) + (m.m[0][0] * m.m[1][2] * m.m[3][1]));
 	result.m[3][3] = (1.0f / a) * ((m.m[0][0] * m.m[1][1] * m.m[2][2]) + (m.m[0][1] * m.m[1][2] * m.m[2][0]) + (m.m[0][2] * m.m[1][0] * m.m[2][1]) - (m.m[0][2] * m.m[1][1] * m.m[2][0]) -
 		(m.m[0][1] * m.m[1][0] * m.m[2][2]) - (m.m[0][0] * m.m[1][2] * m.m[2][1]));
+
+	return result;
+}
+
+Matrix4x4 Transpose(const Matrix4x4& m) {
+	Matrix4x4 result;
+
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result.m[i][j] = m.m[j][i];
+		}
+	}
 
 	return result;
 }
