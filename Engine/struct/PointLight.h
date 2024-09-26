b68563512881a080badc66469b1e68a1f2dcb244
ku-rihara <k023g0027@g.neec.ac.jp>
2024-09-26 18:56:04 +0900

    PointLight実装

--- Engine/struct/PointLight.h
+++ Engine/struct/PointLight.h
@@ -2,7 +2,10 @@
 #include"Vector4.h"
 #include"Vector3.h"
 struct PointLight {
-	Vector4 color;
-	Vector3 position;
-	float intenesity;
+	Vector4 color;//ライトの色
+	Vector3 position;//ライトの位置
+	float intenesity;//輝度
+	float radius;//ライトの幅
+	float decay;//減衰率
+	float padding[2];
 };
