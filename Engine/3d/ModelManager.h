501eed045a8eaf49104742ff31674e5c6608d2ff
ku-rihara <k023g0027@g.neec.ac.jp>
2024-10-23 10:50:39 +0900

    ModelManager変更

--- Engine/3d/ModelManager.h
+++ Engine/3d/ModelManager.h
@@ -1,51 +1,54 @@
 #pragma once
-#include"Model.h"
+#include "Model.h"
+#include <map>
+#include <memory>
+#include <string>
+
 /// <summary>
 /// モデルマネージャー
 /// </summary>
 class ModelManager {
 private:
-	static ModelManager* instance_;
-	std::unique_ptr<ModelCommon> modelCommon = nullptr;
-	//モデルデータ
-	std::map<std::string, std::unique_ptr<Model>>models_;
-	
-	ModelManager() = default;
-	~ModelManager() = default;
-	ModelManager(const ModelManager&) = delete;
-	ModelManager& operator=(const ModelManager&) = delete;
+    static ModelManager* instance_;
+    static std::map<std::string, std::unique_ptr<Model>> models_; // staticに変更
+    std::unique_ptr<ModelCommon> modelCommon = nullptr;
+
+    ModelManager() = default;
+    ~ModelManager() = default;
+    ModelManager(const ModelManager&) = delete;
+    ModelManager& operator=(const ModelManager&) = delete;
+
 public:
-	
-	/// <summary>
-	/// 初期化
-	/// </summary>
-	/// <param name="dxCommon"></param>
-	void Initialize(DirectXCommon* dxCommon);
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    /// <param name="dxCommon"></param>
+    void Initialize(DirectXCommon* dxCommon);
 
-	/// <summary>
-	/// モデルファイル読み込み
-	/// </summary>
-	/// <param name="filePath"></param>
-	/// <param name="extension"></param>
-	void LoadModel(const std::string& modelName, const std::string& extension);
+    /// <summary>
+    /// モデルファイル読み込み (static)
+    /// </summary>
+    /// <param name="modelName"></param>
+    /// <param name="extension"></param>
+    static Model* LoadModel(const std::string& modelName, const std::string& extension);
 
-	/// <summary>
-	/// モデルパーティクルファイル読み込み
-	/// </summary>
-	/// <param name="filePath"></param>
-	/// <param name="extension"></param>
-	void LoadModelParticle(const std::string& modelName, const std::string& extension);
+    /// <summary>
+    /// モデルパーティクルファイル読み込み
+    /// </summary>
+    /// <param name="modelName"></param>
+    /// <param name="extension"></param>
+    void LoadModelParticle(const std::string& modelName, const std::string& extension);
 
-	/// <summary>
-	/// モデルの検索
-	/// </summary>
-	/// <param name="modelName"></param>
-	/// <returns></returns>
-	Model* FindModel(const std::string& modelName, const std::string& extension);
+    /// <summary>
+    /// モデルの検索
+    /// </summary>
+    /// <param name="modelName"></param>
+    /// <returns></returns>
+    Model* FindModel(const std::string& modelName, const std::string& extension);
 
-	//シングルトンインスタンス取得
-	static ModelManager* GetInstance();
-	//終了
-	void Finalize();
+    // シングルトンインスタンス取得
+    static ModelManager* GetInstance();
 
+    // 終了
+    void Finalize();
 };
