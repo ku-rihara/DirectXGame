ac0c232e060035e4d92302d057302d709974804f
ku-rihara <k023g0027@g.neec.ac.jp>
2024-12-23 01:15:19 +0900

    ターゲット座標取得

--- Project/Game/Player/ComboAttackBehavior/ComboAttackFirst.cpp
+++ Project/Game/Player/ComboAttackBehavior/ComboAttackFirst.cpp
@@ -16,7 +16,7 @@ ComboAttackFirst::ComboAttackFirst(Player* player)
 	/// 変数初期化
 	///---------------------------------------------------------
 	startPos_ = pPlayer_->GetRightHand()->GetTransform().translation_;
-	targetPos_ = pPlayer_->GetRightHand()->GetTransform().LookAt(Vector3());
+	targetPos_ = pPlayer_->GetRightHand()->GetTransform().LookAt(Vector3::ToForward());
 
 	
 }
