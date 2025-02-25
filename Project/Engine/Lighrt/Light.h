2a8ea9fc932f46ad29a3bfc664711a409cc6e613
ku-rihara <k023g0027@g.neec.ac.jp>
2025-02-26 01:10:47 +0900

    ライト追加

--- Project/Engine/Lighrt/Light.h
+++ Project/Engine/Lighrt/Light.h
@@ -16,6 +16,7 @@ class DirectionalLight;
 class PointLightManager;
 class SpotLightManager;
 class AreaLightManager;
+class AmbientLight;
 
 class Light {
 private:
@@ -24,7 +25,7 @@ private:
     std::unique_ptr<PointLightManager> pointLightManager_;
     std::unique_ptr<SpotLightManager> spotLightManager_;
     std::unique_ptr<AreaLightManager>areaLightManager_;
-
+    std::unique_ptr<AmbientLight>ambientLight_;
     //虚面反射
     Microsoft::WRL::ComPtr<ID3D12Resource> cameraForGPUResource_;
     //鏡面反射用データ
@@ -43,7 +44,7 @@ public:
     // 各ライトのリソースを取得する関数
     ID3D12Resource* GetDirectionalLightResource() const;
     ID3D12Resource* GetCameraForGPUResources() const;
- 
+
     void SetWorldCameraPos(const Vector3& pos);
 
     // ライトマネージャーを取得するためのメソッド
