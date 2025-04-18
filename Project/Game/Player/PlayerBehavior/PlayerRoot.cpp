4e5ca9fcdb705e53093c65b8c309436ae8c7b9cb
ku-rihara <k023g0027@g.neec.ac.jp>
2025-04-19 01:27:29 +0900

    add animation

--- Project/Game/Player/PlayerBehavior/PlayerRoot.cpp
+++ Project/Game/Player/PlayerBehavior/PlayerRoot.cpp
@@ -15,11 +15,16 @@
 PlayerRoot::PlayerRoot(Player* boss)
 	: BasePlayerBehavior("PlayerRoot", boss) {
 
+    
+
 	/// ===================================================
 	///変数初期化
 	/// ===================================================
+     waitEase_.maxTime = 0.4f;
+    easeDirection_ = 1.0f;
 	speed_ = pPlayer_->GetParamater().moveSpeed;
 
+	animationStep_ = AnimationStep::INIT;
 }
 
 PlayerRoot ::~PlayerRoot() {
@@ -29,7 +34,9 @@ PlayerRoot ::~PlayerRoot() {
 //更新
 void PlayerRoot::Update() {
 
-	
+    // アニメーション
+	MoveAnimation();
+    WaitAnimation();
 
 
 		if ((Input::IsPressPad(0, XINPUT_GAMEPAD_Y))) {
@@ -54,6 +61,81 @@ void PlayerRoot::JumpForJoyState() {
 
 	pPlayer_->ChangeBehavior(std::make_unique<PlayerJump>(pPlayer_));
 		
+}
+
+void PlayerRoot::MoveAnimation() {
+    if (!pPlayer_->GetIsMoving())
+        return;
+
+    switch (animationStep_) {
+    case AnimationStep::INIT:
+        ///============================================================================
+        /// 初期化
+        ///============================================================================
+        animationCollTime_ = 0.5f;
+        moveEase_.time   = 0.0f;
+        waitEase_.time   = 0.0f;
+        moveEase_.maxTime  = 0.4f;
+        moveEase_.amplitude  = 0.4f;
+        moveEase_.period  = 0.2f;
+      
+        animationStep_   = AnimationStep::UPDATE;
+        break;
+    case AnimationStep::UPDATE:
+
+        ///============================================================================
+        /// アニメーション
+        ///============================================================================
+
+        moveEase_.time += Frame::DeltaTime();
+        if (moveEase_.time >= moveEase_.maxTime) {
+            moveEase_.time = 0.0f;
+            animationStep_ = AnimationStep::COOLING;
+        }
+
+        pPlayer_->SetScale(EaseAmplitudeScale<Vector3>(Vector3::UnitVector(), moveEase_.time, moveEase_.maxTime, moveEase_.amplitude, moveEase_.period));
+
+        break;
+    case AnimationStep::COOLING:
+        ///============================================================================
+        /// クールタイム
+        ///============================================================================
+
+        animationCollTime_ -= Frame::DeltaTime();
+        if (animationCollTime_ <= 0.0f) {
+            animationStep_ = AnimationStep::INIT;
+        }
+
+        break;
+    }
+
+
+}
+
+void PlayerRoot::WaitAnimation() {
+
+     if (pPlayer_->GetIsMoving())
+        return;
+
+
+    ///============================================================================
+    /// 待機アニメーション
+    ///============================================================================
+    waitEase_.time += Frame::DeltaTime() * easeDirection_; // 方向に応じて時間を増減
+
+    // タイムが1を超えたら逆方向に、0未満になったら進む方向に変更
+    if (waitEase_.time >= waitEase_.maxTime) {
+        waitEase_.time = waitEase_.maxTime;
+        easeDirection_ = -1.0f; // 逆方向に切り替え
+    }
+
+    else if (waitEase_.time <= 0.0f) {
+        waitEase_.time = 0.0f;
+        easeDirection_ = 1.0f; // 進む方向に切り替え
+    }
+
+    pPlayer_->SetScale(
+        EaseOutBack<Vector3>(Vector3::UnitVector(), {Vector3::UnitVector().x - 0.1f, Vector3::UnitVector().y - 0.1f, Vector3::UnitVector().z - 0.1f}, waitEase_.time, waitEase_.maxTime));
 }
 
 void  PlayerRoot::Debug() {
--- Project/Game/Player/PlayerBehavior/PlayerRoot.h
+++ Project/Game/Player/PlayerBehavior/PlayerRoot.h
@@ -5,17 +5,34 @@
 
 class PlayerRoot : public BasePlayerBehavior {
 private:
+	enum class AnimationStep {
+		INIT,
+		UPDATE,
+		COOLING,
+	};
+
+private:
 
 /// ===================================================
 ///private varians
 /// ===================================================
+
+	AnimationStep animationStep_;
+	float easeDirection_;
 	float speed_;
+
+	float animationCollTime_;
+	Easing waitEase_;
+    Easing moveEase_;
+    float moveEaseTime_;
 	
 public:
 	//コンストラクタ
 	PlayerRoot(Player* boss);
 	~PlayerRoot();
 
+	void MoveAnimation();
+    void WaitAnimation();
 	void Update()override;
 	void Debug()override;
 
