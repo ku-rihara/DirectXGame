6afe2f27b76b6aeb9d71b92fd6e8f5b81df6ab0b
ku-rihara <k023g0027@g.neec.ac.jp>
2025-05-08 12:13:40 +0900

    aabb fix

--- Project/Game/CollisionBox/BaseAABBCollisionBox.h
+++ Project/Game/CollisionBox/BaseAABBCollisionBox.h
@@ -6,34 +6,45 @@
 #include<string>
 
 class BaseAABBCollisionBox :public AABBCollider {
+public:
+
+    /// ====================================================
+    ///  public method
+    /// ====================================================
+
+    virtual void Init();
+    virtual void Update();
+    virtual void Draw();
+
+    virtual void IsAdapt(bool is);
+   
+
+    // Collision発生時のコールバック関数
+    virtual void OnCollisionEnter([[maybe_unused]] BaseCollider* other) override;
+    virtual void OnCollisionStay([[maybe_unused]] BaseCollider* other) override;
+
 protected:
 	/// *TODO 
 	/// Imguiで編集できるエディターを作成
 
 	GlobalParameter* globalParamater_;
+
+	//transform,offset
 	WorldTransform transform_;
-	float scalerOffset_;
 	Vector3 offset_;
+
+    //生存時間
 	float aliveTime_;
 public:
 	std::string tag_;
+
 public:
-
-	virtual void Init();
-	virtual void Update();
-	virtual void Draw();
-
-
-	virtual void IsAdapt(bool is);
-	virtual void SetSize(const Vector3& size);
-	virtual void SetPosition(const Vector3&position);
-	virtual void SetOffset(const Vector3& offset);
-	
-	// 当たり
-	virtual void OnCollisionEnter([[maybe_unused]] BaseCollider* other)override;
-	virtual void OnCollisionStay([[maybe_unused]] BaseCollider* other)override;
-	
+	/// getter
 	virtual Vector3 GetCollisionPos() const override;
-
 	std::string GetTag()const { return tag_; }
+
+    /// setter
+	virtual void SetSize(const Vector3& size);
+    virtual void SetPosition(const Vector3& position);
+    virtual void SetOffset(const Vector3& offset);
 };
