3cd00abef6c9830c99ae3e740815351a74831521
ku-rihara <k023g0027@g.neec.ac.jp>
2025-05-23 14:58:18 +0900

    add particle file

--- Project/Game/Scene/TitleScene.cpp
+++ Project/Game/Scene/TitleScene.cpp
@@ -21,9 +21,9 @@ void TitleScene::Init() {
     BaseScene::Init();
 
     /// パーティクルデータの読み込みと、モデルの読み込み
-    EnemydamageEffect_[0].reset(ParticleEmitter::CreateParticlePrimitive("CylinderParticle", PrimitiveType::Cylinder, 100));
-    EnemydamageEffect_[1].reset(ParticleEmitter::CreateParticlePrimitive("CylinderParticle2", PrimitiveType::Plane, 100));
-    EnemydamageEffect_[2].reset(ParticleEmitter::CreateParticlePrimitive("CylinderParticle3", PrimitiveType::Ring, 100));
+    EnemydamageEffect_[0].reset(ParticleEmitter::CreateParticlePrimitive("StarCenterLight", PrimitiveType::Plane, 100));
+    EnemydamageEffect_[1].reset(ParticleEmitter::CreateParticlePrimitive("StarEffect", PrimitiveType::Plane, 100));
+    EnemydamageEffect_[2].reset(ParticleEmitter::CreateParticlePrimitive("StarFrame", PrimitiveType::Plane, 100));
     ParticleManager::GetInstance()->SetViewProjection(&viewProjection_);
 }
 
@@ -33,7 +33,9 @@ void TitleScene::Update() {
     for (int i = 0; i < EnemydamageEffect_.size(); i++) {
         EnemydamageEffect_[i]->Update();
         EnemydamageEffect_[i]->EditorUpdate();
-        EnemydamageEffect_[i]->Emit();
+        if (Input::GetInstance()->TrrigerKey(DIK_G)) {
+            EnemydamageEffect_[i]->Emit();
+        }
     }
     /* EnemydamageEffect_[0]->Update();
      EnemydamageEffect_[0]->EditorUpdate();
