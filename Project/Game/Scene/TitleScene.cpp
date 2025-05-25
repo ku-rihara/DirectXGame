06fedbcd7dfe74d73b8adc06b2ca69e185a44c9b
ku-rihara <k023g0027@g.neec.ac.jp>
2025-05-25 12:02:17 +0900

    enemy spawn effect

--- Project/Game/Scene/TitleScene.cpp
+++ Project/Game/Scene/TitleScene.cpp
@@ -21,8 +21,8 @@ void TitleScene::Init() {
     BaseScene::Init();
 
     /// パーティクルデータの読み込みと、モデルの読み込み
-    EnemydamageEffect_[0].reset(ParticleEmitter::CreateParticlePrimitive("CylinderParticle", PrimitiveType::Cylinder, 100));
-    EnemydamageEffect_[1].reset(ParticleEmitter::CreateParticlePrimitive("RingParticl", PrimitiveType::Ring, 100));
+    EnemydamageEffect_[0].reset(ParticleEmitter::CreateParticlePrimitive("NormalEnemySpawnCircle", PrimitiveType::Cylinder, 100));
+    EnemydamageEffect_[1].reset(ParticleEmitter::CreateParticlePrimitive("NormalEnemySpawnSpark", PrimitiveType::Plane, 100));
     EnemydamageEffect_[2].reset(ParticleEmitter::CreateParticlePrimitive("PlaneParticl", PrimitiveType::Plane, 100));
     ParticleManager::GetInstance()->SetViewProjection(&viewProjection_);
 }
