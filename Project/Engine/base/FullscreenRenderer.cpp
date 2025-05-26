253c5ebe29172694a4f4352ccc6479a63536fa9d
ku-rihara <k023g0027@g.neec.ac.jp>
2025-05-26 11:07:40 +0900

    fix

--- Project/Engine/base/FullscreenRenderer.cpp
+++ Project/Engine/base/FullscreenRenderer.cpp
@@ -112,6 +112,8 @@ void FullscreenRenderer::Draw(ID3D12GraphicsCommandList* commandList) {
 }
 
 void FullscreenRenderer::DrawImGui() {
+#ifdef DEBUG
+
     if (ImGui::Begin("CopyImageRenderer")) {
         const char* modeNames[] = {"None", "Gray", "Vignette", "Gaus"/*, "RadialBlur"*/};
         int mode                = static_cast<int>(currentMode_);
@@ -120,4 +122,5 @@ void FullscreenRenderer::DrawImGui() {
         }
     }
     ImGui::End();
+#endif // DEBUG
 }
