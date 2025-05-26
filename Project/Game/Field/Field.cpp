f60c7b9e53edf67728eebe577f1fd1a54f023f25
ku-rihara <k023g0027@g.neec.ac.jp>
2025-05-26 10:47:49 +0900

    edit

--- Project/Game/Field/Field.cpp
+++ Project/Game/Field/Field.cpp
@@ -2,8 +2,7 @@
 
 #include "assert.h"
 
-
-Vector3 Field::baseScale_ = {70,1,70};
+Vector3 Field::baseScale_ = {70, 1, 70};
 
 Field::Field() {}
 
@@ -11,19 +10,25 @@ Field::~Field() {}
 
 void Field::Init() {
 
-	obj3d_.reset(Object3d::CreateModel("Field",".obj"));
-	
-	transform_.Init();
-	transform_.scale_ = {1,1,1};
-	obj3d_->material_.materialData_->enableLighting = 5;
+    obj3d_.reset(Object3d::CreateModel("Field", ".obj"));
+
+    transform_.Init();
+    transform_.scale_                               = {1, 1, 1};
+    obj3d_->material_.materialData_->enableLighting = 5;
     obj3d_->material_.SetEnvironmentCoefficient(0.35f);
+
+    emitter_[0].reset(ParticleEmitter::CreateParticlePrimitive("fieldParticleFireSpark", PrimitiveType::Plane, 800));
 }
 
 void Field::Update() {
-	transform_.UpdateMatrix();
+
+    emitter_[0]->Update();
+    emitter_[0]->EditorUpdate();
+    emitter_[0]->Emit();
+
+    transform_.UpdateMatrix();
 }
 
-
 void Field::Draw(ViewProjection& viewProjection) {
-	obj3d_->Draw(transform_, viewProjection);
+    obj3d_->Draw(transform_, viewProjection);
 }
--- Project/Game/Field/Field.h
+++ Project/Game/Field/Field.h
@@ -2,12 +2,17 @@
 #include "3d/Object3d.h"
 #include "3d/WorldTransform.h"
 #include"3d/ViewProjection.h"
+
+#include"utility/ParticleEditor/ParticleEmitter.h"
+#include <array>
 class Field {
 private:
 	// ワールド変換データ
 	WorldTransform transform_;
+
 	// モデル
-	std::unique_ptr<Object3d>obj3d_ = nullptr;
+	 std::unique_ptr<Object3d>obj3d_ = nullptr;
+	 std::array<std::unique_ptr<ParticleEmitter>,1> emitter_;
 
 public:
 
