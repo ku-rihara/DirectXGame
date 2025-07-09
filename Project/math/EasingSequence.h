50f99dedda9452fb1a4a64ada68e8096933c5a59
ku-rihara <k023g0027@g.neec.ac.jp>
2025-07-09 10:56:42 +0900

    easing dequence

---
+++ Project/math/EasingSequence.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Easing.h"
+#include <memory>
+#include <vector>
+
+template <typename T>
+class EasingSequence {
+public:
+    void AddStep(const std::string& name,T*adaptValue);
+    void AddStep(std::unique_ptr<Easing<T>> easing);
+
+    void Reset();
+    void Update(float deltaTime);
+
+    const T& GetValue() const;
+
+private:
+    std::vector<std::unique_ptr<Easing<T>>> steps_;
+    size_t currentStep_ = 0;
+   /* bool finished_      = false;*/
+
+public:
+    const Easing<T>* GetCurrentEasing() const;
+  
+};
