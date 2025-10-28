0916778abda3a2712317be126fe7a47328a7d35a
ku-rihara <k023g0027@g.neec.ac.jp>
2025-10-28 10:52:26 +0900

    easing gix

--- Project/Engine/Easing/Easing.cpp
+++ Project/Engine/Easing/Easing.cpp
@@ -7,13 +7,12 @@
 #include <Windows.h>
 
 template <typename T>
-void Easing<T>::Init( const std::string& adaptFile) {
+void Easing<T>::Init(const std::string& adaptFile) {
 
     // 初期化、ファイル読み込み
     FilePathChangeForType();
     easingFiles_ = GetFileNamesForDyrectry(FilePath_ + filePathForType_);
-    easingName_  = name;
-
+  
     if (!adaptFile.empty()) {
         ApplyFromJson(adaptFile);
         SaveAppliedJsonFileName();
