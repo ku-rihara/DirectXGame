03db0796d946056fee96340df3abe322f4685cda
ku-rihara <k023g0027@g.neec.ac.jp>
2025-11-04 23:57:57 +0900

    file Selectorクラス追加

--- Project/Engine/Function/GetFile.cpp
+++ Project/Engine/Function/GetFile.cpp
@@ -1,14 +1,12 @@
 #include"GetFile.h"
 #include<filesystem>
-///------------------------------------------------------------------------------------------------
-/// パーティクルファイル取得
-///------------------------------------------------------------------------------------------------
-std::vector<std::string> GetFileNamesForDyrectry(const std::string& directory) {
+
+std::vector<std::string> GetFileNamesForDirectory(const std::string& directory) {
     std::vector<std::string> fileNames;
 
     for (const auto& entry : std::filesystem::directory_iterator(directory)) {
         if (entry.is_regular_file()) {  
-            fileNames.push_back(entry.path().stem().string());  // ← ここで拡張子を削除
+            fileNames.push_back(entry.path().stem().string());
         }
     }
     return fileNames;
