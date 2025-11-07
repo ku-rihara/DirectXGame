b5370d8cfb1ada88e2275362941c7cd6bf4bb05c
ku-rihara <k023g0027@g.neec.ac.jp>
2025-11-07 18:02:15 +0900

    CameraAnimationロードできないバグ修正

--- Project/Engine/utility/CameraEditor/CameraAnimation.cpp
+++ Project/Engine/utility/CameraEditor/CameraAnimation.cpp
@@ -24,7 +24,7 @@ void CameraAnimation::Play(const std::string& animationName) {
     currentAnimationName_ = animationName;
     // アニメーションデータの初期化とロード
     animationData_->Init(currentAnimationName_);
-    animationData_->InitKeyFrames();
+    animationData_->LoadData();
 
     // 現在の値を初期値として保存
     SaveInitialValues();
