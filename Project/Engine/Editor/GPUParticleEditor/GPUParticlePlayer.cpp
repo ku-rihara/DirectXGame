5d386d893ca7bc51bf42f46a1efc25ca50f45b4e
ku-rihara <k023g0027@g.neec.ac.jp>
2025-12-08 23:50:43 +0900

    GPUParticle

--- Project/Engine/Editor/GPUParticleEditor/GPUParticlePlayer.cpp
+++ Project/Engine/Editor/GPUParticleEditor/GPUParticlePlayer.cpp
@@ -1,12 +1,24 @@
 #include "GPUParticlePlayer.h"
+#include "GPUParticle/GPUParticleManager.h"
 
 void GPUParticlePlayer::Init() {
     BaseEffectPlayer::Init();
+    isEmitting_     = false;
+    shouldEmitOnce_ = false;
 }
 
 void GPUParticlePlayer::Update(float speedRate) {
     if (effectData_) {
         effectData_->Update(speedRate);
+
+        // 連続エミット中または一度だけエミットする場合
+        if (isEmitting_ || shouldEmitOnce_) {
+            auto* particleData = GetParticleData();
+            if (particleData) {
+                particleData->Draw();
+            }
+            shouldEmitOnce_ = false;
+        }
     }
 }
 
@@ -30,6 +42,11 @@ void GPUParticlePlayer::Play(const std::string& particleName) {
 }
 
 void GPUParticlePlayer::PlayInCategory(const std::string& categoryName, const std::string& particleName) {
+    // 同じパーティクルが既に再生中なら更新のみ
+    if (effectData_ && currentCategoryName_ == categoryName && currentEffectName_ == particleName) {
+        return;
+    }
+
     if (effectData_) {
         effectData_->Pause();
     }
@@ -49,11 +66,57 @@ void GPUParticlePlayer::PlayInCategory(const std::string& categoryName, const st
 }
 
 void GPUParticlePlayer::Draw() {
-    auto* particleData = dynamic_cast<GPUParticleData*>(effectData_.get());
+    auto* particleData = GetParticleData();
     if (particleData) {
         particleData->Draw();
     }
 }
+
+void GPUParticlePlayer::EmitOnce() {
+    shouldEmitOnce_ = true;
+}
+
+void GPUParticlePlayer::StartEmit() {
+    isEmitting_ = true;
+}
+
+void GPUParticlePlayer::StopEmit() {
+    isEmitting_ = false;
+}
+
+void GPUParticlePlayer::SetEmitPosition(const Vector3& position) {
+    auto* particleData = GetParticleData();
+    if (!particleData) {
+        return;
+    }
+
+    // 全セクションのエミット位置を設定
+    for (int i = 0; i < particleData->GetTotalKeyFrameCount(); ++i) {
+        auto* section = particleData->GetKeyFrame(i);
+        if (section) {
+            ParticleEmit emitterData = {};
+            emitterData.translate    = position;
+            section->SetEmitterData(emitterData);
+        }
+    }
+}
+
+void GPUParticlePlayer::SetParentTransform(WorldTransform* parent) {
+    auto* particleData = GetParticleData();
+    if (!particleData) {
+        return;
+    }
+
+    // 全セクションに親トランスフォームを設定
+    for (int i = 0; i < particleData->GetTotalKeyFrameCount(); ++i) {
+        auto* section = particleData->GetKeyFrame(i);
+        if (section) {
+            section->SetParentBasePos(parent);
+        }
+    }
+}
+
+
 
 std::unique_ptr<BaseEffectData> GPUParticlePlayer::CreateEffectData() {
     return std::make_unique<GPUParticleData>();
