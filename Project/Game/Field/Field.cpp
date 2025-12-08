9319b1efa078f9203434d55031cf36be1bc26083
ku-rihara <k023g0027@g.neec.ac.jp>
2025-12-09 01:11:13 +0900

    fall attack edit

--- Project/Game/Field/Field.cpp
+++ Project/Game/Field/Field.cpp
@@ -16,6 +16,6 @@ void Field::Init() {
 
 void Field::Update() {
 
-    gpuParticlePlayer_->Update();
-    gpuParticlePlayer_->PlayInCategory("Field", "Fire");
+    //gpuParticlePlayer_->Update();
+    //gpuParticlePlayer_->PlayInCategory("Field", "Fire");
 }
