3251637581fe2843138d261484a9d948750e11ad
ku-rihara <k023g0027@g.neec.ac.jp>
2026-01-12 23:20:27 +0900

    Editor Scene

--- Project/Game/Scene/EditorScene.h
+++ Project/Game/Scene/EditorScene.h
@@ -1,25 +1,38 @@
 #pragma once
 
-/// baseScene
+/// base
 #include "BaseScene.h"
 
-#include "Easing/Easing.h"
-#include "EasingTestObj/EasingTestObj.h"
-#include "Editor/EasingCreator/Easingeditor.h"
-#include <memory>
+// object
+#include "AttackEffect/AttackEffect.h"
+#include "Audience/AudienceController.h"
+#include "Combo/Combo.h"
+#include "Enemy/EnemyManager.h"
+#include "Enemy/Spawner/EnemySpawner.h"
+#include "Field/Field.h"
+#include "Field/SideRope/SideRopeController.h"
+#include "GameCamera/GameCamera.h"
+#include "LockOn/LockOnController.h"
+#include "Player/ComboCreator/PlayerComboAttackController.h"
+#include "Player/Player.h"
+#include "SkyBox/SkyBox.h"
+#include "SkyDome/SkyDome.h"
+
+#include "utility/ObjectFromBlender/ObjectFromBlender.h"
 
 /// <summary>
-/// エディターシーン
+/// ゲームシーン
 /// </summary>
 class EditorScene : public BaseScene {
-private:
-    std::unique_ptr<EasingTestObj> easingTestObject_;
-    KetaEngine::EasingEditor easingEditor_;
-
 public:
-    EditorScene();
-    ~EditorScene() override;
+    EditorScene()           = default;
+    ~EditorScene() override = default;
 
+    ///========================================================
+    /// public method
+    ///========================================================
+
+    /// 初期化、更新、描画
     void Init() override;
     void Update() override;
     void SkyBoxDraw() override;
@@ -27,4 +40,33 @@ public:
     void Debug() override;
     void ViewProjectionUpdate() override;
     void ViewProcess() override;
+
+private:
+    ///========================================================
+    /// private method
+    ///========================================================
+
+    void ObjectInit();
+    void SetClassPointer();
+
+private:
+    //*-------------------------------- Private variants--------------------------------*//
+
+    std::unique_ptr<GameCamera> gameCamera_                                   = nullptr;
+    std::unique_ptr<Field> field_                                             = nullptr;
+    std::unique_ptr<LockOnController> lockOnController_                       = nullptr;
+    std::unique_ptr<Player> player_                                           = nullptr;
+    std::unique_ptr<SkyDome> skyDome_                                         = nullptr;
+    std::unique_ptr<SkyBox> skyBox_                                           = nullptr;
+    std::unique_ptr<Combo> combo_                                             = nullptr;
+    std::unique_ptr<EnemyManager> enemyManager_                               = nullptr;
+    std::unique_ptr<EnemySpawner> enemySpawner_                               = nullptr;
+    std::unique_ptr<AttackEffect> attackEffect_                               = nullptr;
+    std::unique_ptr<PlayerComboAttackController> playerComboAttackController_ = nullptr;
+    std::unique_ptr<SideRopeController> sideRopeController_                   = nullptr;
+    std::unique_ptr<AudienceController> audienceController_                   = nullptr;
+
+    std::unique_ptr<KetaEngine::ObjectFromBlender> ObjectFromBlender_ = nullptr;
+
+public:
 };
