774a11103b665093e6e9bec9e15d4f3d4254c55c
ku-rihara <k023g0027@g.neec.ac.jp>
2026-05-09 15:23:08 +0900

    死亡中にゲームオーバーになると固まるバグ修正

--- Project/Game/Player/Player.cpp
+++ Project/Game/Player/Player.cpp
@@ -444,7 +444,14 @@ void Player::ResetHeadScale() {
 
 bool Player::IsAbleBehavior() {
     bool isAttackRoot = (dynamic_cast<ComboAttackRoot*>(comboBehavior_.get()));
-    return (isAttackRoot);
+    
+    // 攻撃中かつ死んでいないなら攻撃挙動のみ可能（既存の仕様）
+    if (isAttackRoot && !(isDeath_ && *isDeath_)) {
+        return true;
+    }
+
+    // それ以外（死んでいる、または攻撃中でない）は通常挙動を許可する
+    return !isAttackRoot;
 }
 
 void Player::InitInGameScene() {
